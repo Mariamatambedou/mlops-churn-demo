fec7729e60ee25f5af5123ecf2cca2f7d15dda25
mariama <mariama.tambedou@baamtu.com>
2026-02-17 18:05:02 +0000

    Load production model from W&B registry

--- api/main.py
+++ api/main.py
@@ -1,40 +1,50 @@
-from pathlib import Path
-import joblib
-import pandas as pd
 from fastapi import FastAPI
 from pydantic import BaseModel
+import pandas as pd
+import joblib
+import wandb
+from pathlib import Path
 
 app = FastAPI(title="Churn Prediction API")
 
-MODEL_PATH = Path("models/model.pkl")
-SCALER_PATH = Path("data/processed/scaler.pkl")
+# -----------------------------
+# Load model from W&B Registry
+# -----------------------------
+WANDB_PROJECT = "churn-mlops"
+WANDB_ENTITY = "tambedou89mariama-baamtu"
 
-model = joblib.load(MODEL_PATH)
-scaler = joblib.load(SCALER_PATH)
+run = wandb.init(
+    project=WANDB_PROJECT,
+    entity=WANDB_ENTITY,
+    job_type="inference"
+)
 
+artifact = run.use_artifact(
+    "churn_model:production",
+    type="model"
+)
 
+artifact_dir = artifact.download()
 
+model = joblib.load(Path(artifact_dir) / "model.pkl")
+
+# -----------------------------
+# Input schema
+# -----------------------------
 class Customer(BaseModel):
     tenure: int
     monthly_charges: float
     total_charges: float
 
-
+# -----------------------------
+# Routes
+# -----------------------------
 @app.get("/")
 def health():
     return {"status": "ok"}
 
-
 @app.post("/predict")
 def predict(data: Customer):
-
     df = pd.DataFrame([data.dict()])
-
-    # scaling comme en training
-    X_scaled = scaler.transform(df)
-
-    proba = model.predict_proba(X_scaled)[0][1]
-
-    return {
-        "churn_probability": round(float(proba), 4)
-    }
+    proba = model.predict_proba(df)[0][1]
+    return {"churn_probability": round(float(proba), 4)}
